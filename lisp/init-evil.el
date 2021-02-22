b0bf96f95a49b07fc09df383d3c677e4c8aec2c1
Ivan Chien <qyc027@gmail.com>
2021-02-22 20:49:40 +0800

    Add evil-mode

---
+++ lisp/init-evil.el
@@ -0,0 +1,10 @@
+;;; init-git.el --- Git SCM support -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require-package 'evil)
+
+(global-set-key (kbd "M-s e") 'turn-on-evil-mode)
+(global-set-key (kbd "M-s E") 'turn-off-evil-mode)
+
+;;; init-evil.el ends here
