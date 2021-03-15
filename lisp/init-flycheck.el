d6a2d79191fd8bb6271a9f4a079524dfb9c01f98
Ivan Chien <qyc027@gmail.com>
2021-03-15 09:50:46 +0800

    Add flycheck C and C++ standard
    - set flycheck-clang-language-standard to c17 for c-mode
    - set flycheck-clang-language-standard to c++17 for c++-mode

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -9,6 +9,13 @@
   (when (maybe-require-package 'flycheck-color-mode-line)
     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
 
+;; C standard
+(add-hook 'c-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c17")))
+
+;; C++ standard
+(add-hook 'c++-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c++17")))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
