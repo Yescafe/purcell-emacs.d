e20fed6053d98d194248c16c94a951dfbaee2783
Ivan Chien <qyc027@gmail.com>
2021-03-17 08:38:14 +0800

    Update init-term comments

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -2,15 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
-;; thanks https://laptrinhx.com/customizable-256-colors-for-emacs-term-and-ansi-term-3643817902/
+;; refs https://laptrinhx.com/customizable-256-colors-for-emacs-term-and-ansi-term-3643817902/ thanx
 
+;; requirements
 (require-package 'eterm-256color)
 
+;; ansi-color ---> eterm-256color
 (add-hook 'term-mode-hook #'eterm-256color-mode)
+
+;; disable line number display in ansi-term
 (add-hook 'term-mode-hook
           (lambda ()
             (display-line-numbers-mode 0)))
 
+
+;; set term global key
 (global-set-key (kbd "C-c t") 'ansi-term)
 
 
