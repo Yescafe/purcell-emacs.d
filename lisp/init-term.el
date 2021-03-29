d06b278dbe4e75ed0254f352c8bb6431f5e1a273
Ivan Chien <qyc027@gmail.com>
2021-03-29 15:40:15 +0800

    Add zsh function

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -20,5 +20,26 @@
 (global-set-key (kbd "C-c t") 'ansi-term)
 
 
+;; refs https://emacs.stackexchange.com/questions/48472/how-to-make-m-x-ansi-term-behave-like-m-x-shell-opening-in-new-window
+;; Open ansi-term/zsh on the righter buffer
+
+;; Support window mode for zsh
+(defun ns/my-term-p (name _action)
+  "Determine whether NAME names a `term-mode' buffer."
+  (with-current-buffer name
+    (derived-mode-p #'term-mode)))
+
+;; Open zsh directly
+(defun zsh ()
+  (interactive)
+  (let ((switch-to-buffer-obey-display-actions t))
+    (ansi-term "zsh")))
+
+(add-to-list 'display-buffer-alist '(ns/my-term-p () (inhibit-same-window . t)))
+
+;; Set zsh key binding
+(global-set-key (kbd "C-c t") 'zsh)
+
+
 (provide 'init-term)
 ;;; init-term.el ends here
