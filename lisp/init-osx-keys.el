d6a3cdaabf15e3533756bee7c99b492412045967
Ivan Chien <qyc027@gmail.com>
2021-08-20 15:51:15 +0800

    Update osx-keys, add s-x and s-c

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -21,6 +21,8 @@
 
   (global-set-key (kbd "s-a") 'mark-whole-buffer)
   (global-set-key (kbd "s-s") 'save-buffer)
+  (global-set-key (kbd "s-x") 'whole-line-or-region-kill-region)
+  (global-set-key (kbd "s-c") 'whole-line-or-region-kill-ring-save)
   (global-set-key (kbd "s-v") 'yank)
   (global-set-key (kbd "s-z") 'undo-fu-only-undo)
   (global-set-key (kbd "s-Z") 'undo-fu-only-redo)
